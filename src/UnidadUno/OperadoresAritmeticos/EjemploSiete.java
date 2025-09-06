8d713afd500fbef0f2024bb12e7f1ece89aae957
juangar <jdgarciaam@ufpso.edu.co>
2025-09-05 20:46:54 -0500

    EjemploSeis

---
+++ src/UnidadUno/OperadoresAritmeticos/EjemploSiete.java
@@ -0,0 +1,10 @@
+package UnidadUno.OperadoresAritmeticos;
+
+public class EjemploSiete {
+    public static void main(String[] args) {
+        int b = 10;
+        b -= 2; 
+        System.out.println(b);
+    }
+    
+}
