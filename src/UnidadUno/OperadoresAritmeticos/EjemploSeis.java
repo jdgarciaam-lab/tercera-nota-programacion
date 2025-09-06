488390d0559da8469e2c772d43288da9de67d5ef
juangar <jdgarciaam@ufpso.edu.co>
2025-09-05 20:41:39 -0500

    EjemploSeis

---
+++ src/UnidadUno/OperadoresAritmeticos/EjemploSeis.java
@@ -0,0 +1,9 @@
+package UnidadUno.OperadoresAritmeticos;
+
+public class EjemploSeis {
+    public static void main(String[] args) {
+      int a = 5;
+     a += 3;   
+     System.out.println(a);
+    }
+}
