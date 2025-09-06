5eb6eb8eee911c6e5419c3e1ab04c83ab52f5192
juangar <jdgarciaam@ufpso.edu.co>
2025-09-05 20:49:45 -0500

    EjeemploOcho

---
+++ src/UnidadUno/OperadoresAritmeticos/EjemploOcho.java
@@ -0,0 +1,10 @@
+package UnidadUno.OperadoresAritmeticos;
+
+public class EjemploOcho {
+    public static void main(String[] args) {
+        int c = 4;
+        c *= 3;
+        System.out.println(c);
+    }
+    
+}
