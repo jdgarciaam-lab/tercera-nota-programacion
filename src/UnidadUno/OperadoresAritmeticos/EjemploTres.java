3cb5edf9d3073b89eb614941e9e4e8a07d4ef095
juangar <jdgarciaam@ufpso.edu.co>
2025-09-05 20:28:22 -0500

    EjemploTres

---
+++ src/UnidadUno/OperadoresAritmeticos/EjemploTres.java
@@ -0,0 +1,8 @@
+package UnidadUno.OperadoresAritmeticos;
+
+public class EjemploTres {
+    public static void main(String[] args) {
+        int resultado = 6 * 7;
+        System.out.println(resultado);
+    }
+}
